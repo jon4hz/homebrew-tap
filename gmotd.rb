b1f25a722121c98d0c6c7626ca30cf961a0feeff
jon4hz <me@jon4hz.io>
2024-03-01 20:54:21 +0100

    Brew formula update for gmotd version v0.2.0

--- gmotd.rb
+++ gmotd.rb
@@ -5,12 +5,12 @@
 class Gmotd < Formula
   desc "a configurable, dynamic motd generator"
   homepage "https://jon4hz.io"
-  version "0.1.0"
+  version "0.2.0"
   depends_on :linux
 
   if Hardware::CPU.intel?
-    url "https://github.com/jon4hz/gmotd/releases/download/v0.1.0/gmotd-v0.1.0-linux-amd64.tar.gz"
-    sha256 "b7df3b57e628ac93b736b52e09ca39228f7b1fca160d5d3e3b992e7ab894fb51"
+    url "https://github.com/jon4hz/gmotd/releases/download/v0.2.0/gmotd-v0.2.0-linux-amd64.tar.gz"
+    sha256 "27543949fe14ac28491cdf5a496df87f69db57403aff3bb33ad56b60c732b903"
 
     def install
       bin.install "gmotd"
