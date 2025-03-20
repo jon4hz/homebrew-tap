a71777250c35320ad67bb90fd23aa898631cdf7f
jon4hz <me@jon4hz.io>
2025-03-20 19:34:24 +0100

    Brew formula update for canihazgnomeupdate version v0.1.3

--- canihazgnomeupdate.rb
+++ canihazgnomeupdate.rb
@@ -5,15 +5,17 @@
 class Canihazgnomeupdate < Formula
   desc "check if all your extensions support a specific gnome version "
   homepage "https://jon4hz.io"
-  version "0.1.2"
+  version "0.1.3"
   depends_on :linux
 
   if Hardware::CPU.intel?
-    url "https://github.com/jon4hz/canihazgnomeupdate/releases/download/v0.1.2/canihazgnomeupdate-v0.1.2-linux-amd64.tar.gz"
-    sha256 "f962509b62ae67af88f979a045c83aea566e9fe9ab06745998e4be48bcf8dd29"
+    if Hardware::CPU.is_64_bit?
+      url "https://github.com/jon4hz/canihazgnomeupdate/releases/download/v0.1.3/canihazgnomeupdate-v0.1.3-linux-amd64.tar.gz"
+      sha256 "668c4c9f7747b1098e968a3b86dc0fe2eb1b53e7bc77d13c59d96e3fb8b5ab76"
 
-    def install
-      bin.install "canihazgnomeupdate"
+      def install
+        bin.install "canihazgnomeupdate"
+      end
     end
   end
 end
